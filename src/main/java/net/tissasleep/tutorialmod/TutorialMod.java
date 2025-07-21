ae6e0d5ff0ed73ef3a79b4cc845e34ac990402d9
tiss-asleep <jambalm0674@gmail.com>
2025-07-21 17:47:30 +0800

    pink garnet and raw pink garnet added

--- src/main/java/net/tissasleep/tutorialmod/TutorialMod.java
+++ src/main/java/net/tissasleep/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.tissasleep.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tissasleep.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
